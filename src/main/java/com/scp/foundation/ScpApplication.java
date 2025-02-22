714c9adb422931608fe8d8e767e9a37bc769e465
xhackax47 <xhackax47@gmail.com>
2025-02-22 21:06:36 +0100

    [013] Retrait de l'annotation RestController sur le point d'entrée de l'application (ScpApplication)

--- src/main/java/com/scp/foundation/ScpApplication.java
+++ src/main/java/com/scp/foundation/ScpApplication.java
@@ -2,10 +2,8 @@ package com.scp.foundation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class ScpApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ScpApplication.class, args);
