8d65f73d0cfe60a34773053f534af469f7e5e435
xhackax47 <xhackax47@gmail.com>
2025-02-26 11:05:37 +0100

    [023] Fix sur les entitées SCP (id généré)

--- src/main/java/com/scp/foundation/models/scp/KeterSCP.java
+++ src/main/java/com/scp/foundation/models/scp/KeterSCP.java
@@ -1,5 +1,7 @@
 package com.scp.foundation.models.scp;
 
+import java.util.Objects;
+
 import com.scp.foundation.enums.securitylevels.SCPSecurityLevel;
 
 import jakarta.persistence.Entity;
@@ -9,10 +11,13 @@ import jakarta.persistence.Id;
 
 @Entity
 public final class KeterSCP extends SCP {
-
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id;
+	protected Long id;
+	protected String name;
+	protected String description;
+	protected SCPSecurityLevel securityLevel;
 
 	public KeterSCP() {
 		super();
@@ -25,4 +30,54 @@ public final class KeterSCP extends SCP {
 		this.securityLevel = securityLevel;
 	}
 
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public SCPSecurityLevel getSecurityLevel() {
+		return securityLevel;
+	}
+
+	public void setSecurityLevel(SCPSecurityLevel securityLevel) {
+		this.securityLevel = securityLevel;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(description, id, name, securityLevel);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ThaumielSCP other = (ThaumielSCP) obj;
+		return Objects.equals(description, other.description) && Objects.equals(id, other.id)
+				&& Objects.equals(name, other.name) && securityLevel == other.securityLevel;
+	}
+
+	@Override
+	public String toString() {
+		return "SCP [name=" + name + ", description=" + description + ", securityLevel=" + securityLevel + "]";
+	}
 }
--- src/main/java/com/scp/foundation/models/scp/SCP.java
+++ src/main/java/com/scp/foundation/models/scp/SCP.java
@@ -5,71 +5,9 @@ import java.util.Objects;
 import com.scp.foundation.enums.securitylevels.SCPSecurityLevel;
 
 public sealed class SCP permits AppolyonSCP, EuclidSCP, KeterSCP, SafeSCP, ThaumielSCP {
-	protected Long id;
-	protected String name;
-	protected String description;
-	protected SCPSecurityLevel securityLevel;
-
+	
 	public SCP() {
 		super();
 	}
 
-	public SCP(Long id, String name, String description, SCPSecurityLevel securityLevel) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.description = description;
-		this.securityLevel = securityLevel;
-	}
-	
-	public Long getId() {
-		return id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public SCPSecurityLevel getSecurityLevel() {
-		return securityLevel;
-	}
-
-	public void setSecurityLevel(SCPSecurityLevel securityLevel) {
-		this.securityLevel = securityLevel;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(description, name, securityLevel);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		SCP other = (SCP) obj;
-		return Objects.equals(description, other.description) && Objects.equals(name, other.name)
-				&& securityLevel == other.securityLevel;
-	}
-
-	@Override
-	public String toString() {
-		return "SCP [name=" + name + ", description=" + description + ", securityLevel=" + securityLevel + "]";
-	}
 }
