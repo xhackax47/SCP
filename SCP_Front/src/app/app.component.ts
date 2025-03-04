396081c7cc65ce0ce025794fee0b378ac0e0d69e
xhackax47 <xhackax47@gmail.com>
2025-03-04 17:58:40 +0100

    [035] Front - Ajout bootstrap et fontawesome + Dossier UI + Rework de la page home avec logo SCP et navbar

--- SCP_Front/src/app/app.component.ts
+++ SCP_Front/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {HomeComponent} from './home/home.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, HomeComponent],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.less'
+  templateUrl: 'home/home.component.html',
+  styleUrl: 'home/home.component.less'
 })
 export class AppComponent {
   title = 'SCP Foundation';
